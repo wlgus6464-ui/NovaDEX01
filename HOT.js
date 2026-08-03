84a3a3b86051edfd278134dddce3065419e700b4
wlgus6464-ui <wlgus6464@gmail.com>
2026-08-03 16:15:58 +0900

    Update HOT.js

--- HOT.js
+++ HOT.js
@@ -3,16 +3,16 @@ const hotChains = [
     {
         name: "Momentum (MMT)",
         labels: ["Jul 28","Jul 29","Jul 30","Jul 31","Aug 1","Aug 2","Aug 3"],
-        netflow: [-1.0, 3.0, 1.4, 0.7, 0.1, -0.5, -1.2],
+        netflow: [-1.0, 3.0, 1.4, 0.7, 0.1, -0.5, 0.2],
         reserve: [42.1, 43.0, 41.9, 40.8, 40.2, 41.7, 40.5],
-        goToLink: "https://www.upbit.com/exchange?code=CRIX.UPBIT.KRW-SOL"
+        goToLink: "https://www.upbit.com/exchange?code=CRIX.UPBIT.KRW-MMT"
     },
     {
-        name: "Sui (SUI)",
+        name: "MetaDAO (META2)",
         labels: ["Jul 28","Jul 29","Jul 30","Jul 31","Aug 1","Aug 2","Aug 3"],
-        netflow: [-0.4, 1.2, 0.8, -1.5, 2.0, 1.1, 0.3],
-        reserve: [1.85, 1.88, 1.82, 1.91, 1.95, 1.90, 1.98],
-        goToLink: "https://www.upbit.com/exchange?code=CRIX.UPBIT.KRW-SUI"
+        netflow: [-0.4, -1.2, -0.8, 1.5, 0.2, -0.7, -0.3],
+        reserve: [1.85, 1.88, 1.82, 1.91, 1.75, 1.69, 1.71],
+        goToLink: "https://www.upbit.com/exchange?code=CRIX.UPBIT.KRW-META2"
     },
     {
         name: "Zilliqa (ZIL)",
