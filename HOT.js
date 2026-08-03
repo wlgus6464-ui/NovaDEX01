81deca288d0ad73b479365c0985b932830770fce
wlgus6464-ui <wlgus6464@gmail.com>
2026-08-03 16:21:18 +0900

    Update HOT.js

--- HOT.js
+++ HOT.js
@@ -17,7 +17,7 @@ const hotChains = [
     {
         name: "Definitive (EDGE)",
         labels: ["Jul 28","Jul 29","Jul 30","Jul 31","Aug 1","Aug 2","Aug 3"],
-        netflow: [0.6, -1.1, -0.5, -0.8, -0.2, 0.0, 1.3],
+        netflow: [0.6, -1.1, -0.5, -0.8, -0.2, 0.0, 3.0],
         reserve: [5.12, 5.05, 5.09, 5.14, 5.11, 5.07, 5.28],
         goToLink: "https://www.upbit.com/exchange?code=CRIX.UPBIT.KRW-EDGE"
     }
