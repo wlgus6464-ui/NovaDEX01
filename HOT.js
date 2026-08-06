b2285478dbf6fc5191ed18c7668294eed1b34afa
wlgus6464-ui <wlgus6464@gmail.com>
2026-08-06 10:28:42 +0900

    Update HOT.js

--- HOT.js
+++ HOT.js
@@ -1,24 +1,24 @@
 // HOT.js
 const hotChains = [
     {
-        name: "Momentum (MMT)",
-        labels: ["Jul 28","Jul 29","Jul 30","Jul 31","Aug 1","Aug 2","Aug 3"],
-        netflow: [-1.0, 3.0, 1.4, 0.7, 0.1, -0.5, 0.2],
-        reserve: [42.1, 43.0, 41.9, 40.8, 40.2, 41.7, 40.5],
-        goToLink: "https://www.upbit.com/exchange?code=CRIX.UPBIT.KRW-MMT"
-    },
-    {
         name: "MetaDAO (META2)",
-        labels: ["Jul 28","Jul 29","Jul 30","Jul 31","Aug 1","Aug 2","Aug 3"],
-        netflow: [-0.4, -1.2, -0.8, 1.5, 0.2, -0.7, -0.3],
-        reserve: [1.85, 1.88, 1.82, 1.91, 1.75, 1.69, 1.71],
+        labels: ["Jul 31","Aug 1","Aug 2","Aug 3","Aug 4","Aug 5","Aug 6",],
+        netflow: [ 1.5, 0.2, -0.7, -0.3, -0.7, -0.3, 0.1, 0.1],
+        reserve: [1.91, 1.75, 1.69, 1.71, 1.65, 1.57, 1.50],
         goToLink: "https://www.upbit.com/exchange?code=CRIX.UPBIT.KRW-META2"
     },
     {
+        name: "Biconomy (BICO)",
+        labels: ["Jul 31","Aug 1","Aug 2","Aug 3","Aug 4","Aug 5","Aug 6",],
+        netflow: [-0.8, -0.2, 0.0, 3.0, 2.8, 2.0, 2.3],
+        reserve: [5.14, 5.11, 5.07, 5.28, 5.19, 5.22, 5.01],
+        goToLink: "https://www.bithumb.com/react/trade/order/BICO-KRW"
+    },
+    {
         name: "Definitive (EDGE)",
-        labels: ["Jul 28","Jul 29","Jul 30","Jul 31","Aug 1","Aug 2","Aug 3"],
-        netflow: [0.6, -1.1, -0.5, -0.8, -0.2, 0.0, 3.0],
-        reserve: [5.12, 5.05, 5.09, 5.14, 5.11, 5.07, 5.28],
+        labels: ["Jul 31","Aug 1","Aug 2","Aug 3","Aug 4","Aug 5","Aug 6",],
+        netflow: [-0.6, -1.1, -0.5, -0.8, -0.5, 1.0, 2.5],
+        reserve: [15.27, 15.05, 15.11, 15.14, 15.11, 15.31, 15.50],
         goToLink: "https://www.upbit.com/exchange?code=CRIX.UPBIT.KRW-EDGE"
     }
 ];
