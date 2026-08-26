c8849b02a803c0d815edba295f8c73de167fcf87
wlgus6464-ui <wlgus6464@gmail.com>
2026-08-26 10:49:58 +0900

    Update trendingData.js

--- trendingData.js
+++ trendingData.js
@@ -31,7 +31,7 @@ const trendingData = [
         prediction: "+571%",
         bannerImage: "https://coingape.com/wp-content/uploads/2025/05/Why-Kaito-Token-Price-Exploded-50-Today-Whats-Next.webp",
         bannerText: "The Pudgy Penguins ecosystem has recently experienced large price volatility and increased trading volumes, which on-chain data has attributed to both underlying growth and technical market mechanisms. While some analysts point to the timing of the token unlocking as the main reason for the recent liquidity, others are highlighting the project's recent expansion into the mainstream financial and gaming sectors. The PENGU token, a cryptocurrency endemic to the Pudgy Penguins ecosystem, is up more than 30% over the past month since hitting a 12-month low earlier this year.",
-        participateLink: "https://www.upbit.com/exchange?code=CRIX.UPBIT.KRW-KAITO"
+        participateLink: "https://www.upbit.com/exchange?code=CRIX.UPBIT.KRW-DOS"
     },
     {
         img: "https://img.magnific.com/free-vector/modern-coming-soon-new-arrival-background-with-spot-light-effect_1017-57496.jpg?semt=ais_hybrid&w=740&q=80",
